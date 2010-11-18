31d06b3cb0b58a93143c49f8e407b1258e63293d
SixArm <sixarm@sixarm.com>
2010-11-17 18:30:01 -0800

    add icons

--- app/helpers/views_helper.rb
+++ app/helpers/views_helper.rb
@@ -1,35 +1,46 @@
 module ViewsHelper
 
+  H1_OPEN = "<h1>".html_safe
+  H1_SHUT = "</h1>".html_safe
+  H2_OPEN = "<h2>".html_safe
+  H2_SHUT = "</h2>".html_safe
+  H3_OPEN = "<h3>".html_safe
+  H3_SHUT = "</h3>".html_safe
+
   def h1(sym)
-    "<h1>#{translate(sym)}</h1>"
+    H1_OPEN + translate(sym) + H1_SHUT
   end
 
   def h2(sym)
-    "<h2>#{translate(sym)}</h2>"
+    H2_OPEN + translate(sym) + H2_SHUT
   end
 
   def h3(sym)
-    "<h3>#{translate(sym)}</h3>"
+    H3_OPEN + translate(sym) + H3_SHUT
+  end
+
+  def icon(file_name_key,alt_text_key)
+    return "<img border=\"0\" width=\"16\" height=\"16\" src=\"/images/icons/#{file_name_key}.png\" alt=\"#{translate(alt_text_key)}\"/>".html_safe
+  end
+
+  def link_to_create(ops={})
+    link_to(icon(:create, translate(:Create)), ops)
   end
 
   def link_to_back(ops={})
-    link_to(translate(:Back), ops)
+    link_to(icon(:back, translate(:Back)), ops)
   end
 
-  def link_to_destroy(obj)
-    link_to(translate(:Destroy), obj, :confirm => translate(:Destroy_Confirm), :method => :delete)
+  def link_to_delete(obj)
+    link_to(icon(:delete, translate(:Delete)), obj, :confirm => translate(:Delete_Confirm), :method => :delete)
   end
 
   def link_to_edit(ops={})
-    link_to(translate(:Edit), ops)
-  end
-
-  def link_to_new(ops={})
-    link_to(translate(:New), ops)
+    link_to(icon(:edit, translate(:Edit)), ops)
   end
 
   def link_to_show(ops={})
-    link_to(translate(:Show), ops)
+    link_to(icon(:show, translate(:Show)), ops)
   end
 
 end
