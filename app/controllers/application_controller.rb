5b717771c6e5f7adb61cc88badc7b00733a2d31d
SixArm <sixarm@sixarm.com>
2010-12-12 20:58:13 -0800

    update controllers with inherited resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :application_layout
+
+  private
+  def application_layout
+    request.host().sub(/^a\./,'')
+  end
+
 end
